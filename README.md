dde3aa50cbb3c91a95217df7b7b5aeca05112eaf
Upsided <matthewlloydw@gmail.com>
2022-03-02 05:36:33 +0000

    Updated README and Jsdoc

---
+++ README.md
@@ -0,0 +1,6 @@
+# djs-utils
+
+[Discord.js](https://discord.js.org/#/) related function(s) to make my life easier.
+## functions
+- [disableRow](https://github.com/Upsidedly/djs-utils/blob/master/src/index.js#L10) 
+    - Takes a [MessageActionRow](https://discord.js.org/#/docs/main/stable/class/MessageActionRow) and returns it but with all [components](https://discord.js.org/#/docs/main/stable/typedef/MessageActionRowComponentResolvable) disabled. 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import { MessageActionRow } from "discord.js";
 
 /**
- * Takes a MessageActionRow and returns it with all components disabled.
+ * Takes a [MessageActionRow](https://discord.js.org/#/docs/main/stable/class/MessageActionRow)
+ * and returns it with all [components](https://discord.js.org/#/docs/main/stable/typedef/MessageActionRowComponentResolvable) disabled.
  * @param {MessageActionRow} row 
  * The MessageActionRow
  * @returns {MessageActionRow}
